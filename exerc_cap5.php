e4f357958a3ca1fbbd3f6104a0dec491faae6dde
mrcsz <marss@ic.ufal.br>
2018-02-26 01:16:09 -0300

    moved for dir exercCap4e5

--- exerc_cap5.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'functions.php';
-
-capture_img('abacaxi.jpg', 'abacaxi', 120, 120);
-capture_img('cat.jpg', 'gato', 120, 120);
-capture_img('coelho.jpg', 'coelho', 120, 120);
-capture_img('dog.jpg', 'cachorro', 120, 120);
-capture_img('luz.jpg', 'lampada', 120, 120);
